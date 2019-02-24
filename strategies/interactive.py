b72d3306f2218657a23d308481d17caa4609a380
Tom curnock <curnock@gmail.com>
2019-02-24 11:37:38 -0500

    add strategies

---
+++ strategies/interactive.py
@@ -0,0 +1,16 @@
+import hypercube as hc
+from strategy import dec_strategy, Strategy
+
+Cell_coord = hc.Cell_coord
+
+@dec_strategy
+class Interactive(Strategy):
+    
+    def __init__(self, d: int, n: int, moves_per_turn = 1, drop = False) -> None:
+        super().__init__(d, n, moves_per_turn, drop)
+
+    def move(self, cell: Cell_coord) -> Cell_coord:
+        pass
+
+    def undo(self, cell: Cell_coord) -> Cell_coord:
+        pass
